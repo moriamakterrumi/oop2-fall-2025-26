6982fee272af1f8c58689a6ce56a6453d944aba8
moriamakterrumi <moriam657@gmail.com>
2025-10-31 18:48:18 +0600

    Update Program.cs

--- Task 1/Task 1/Program.cs
+++ Task 1/Task 1/Program.cs
@@ -6,4 +6,4 @@ Console.Write("Enter third number : ");
 double num3 = Convert.ToDouble(Console.Readline());
 
 double average = (num1 + num2 + num3) / 3.0;
-Console.WriteLine($"Average Score: {average}");
+Console.WriteLine($"Average Score: {average:F2}");
