48a06a8e9942f8e6ac95dc55b3a5b4f952fc104b
moriamakterrumi <moriam657@gmail.com>
2025-12-15 14:39:26 +0600

    23-51190-1

---
+++ Lab/Interface/Interface/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MultipleInterfaceDemo
+{
+    
+    interface IPrintable
+    {
+        void Print();
+    }
+
+    
+    interface ISerializable
+    {
+        void Save();
+    }
+
+
+    class Report : IPrintable, ISerializable
+    {
+        public void Print()
+        {
+            Console.WriteLine("Printing the report...");
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saving the report...");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Report report = new Report();
+
+            report.Print();
+            report.Save();
+
+            
+        }
+    }
+}
---
+++ Lab/Student Record System/Student Record System/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+interface IStudent
+
+{
+    void Display();
+
+}
+class Student : IStudent
+
+{
+    private int[] marks = new int[3];
+
+    private string name;
+    public Student(string name)
+
+    {
+
+        this.name = name;
+
+    }
+    public int this[int index]
+
+    {
+        get
+
+        {
+
+            return marks[index];
+
+        }
+
+        set
+
+        {
+
+            marks[index] = value;
+
+        }
+
+    }
+
+    public void Display()
+
+    {
+
+        Console.WriteLine("Student Name: " + name);
+
+        Console.WriteLine("Marks:");
+
+        for (int i = 0; i < marks.Length; i++)
+
+        {
+
+            Console.WriteLine("Subject " + (i + 1) + ": " + marks[i]);
+
+        }
+    }
+}
+
+class Program
+
+{
+
+    static void Main()
+
+    {
+        Student s = new Student("Rumi");
+
+        s[0] = 80;
+
+        s[1] = 85;
+
+        s[2] = 90;
+
+        s.Display();
+
+    }
+
+}
