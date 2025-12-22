d049023b85173ac46c8cd46ff0f3683d6d6c962f
moriamakterrumi <moriam657@gmail.com>
2025-12-22 14:43:04 +0600

    23-51190-1

---
+++ Lab/Delegates/Delegates/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DelegateAsParameterDemo
+{
+ 
+    public delegate int Operation(int a, int b);
+
+    class Program
+    {
+       
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+    
+        static void Calculate(int a, int b, Operation op)
+        {
+            int result = op(a, b);  
+            Console.WriteLine("Result: " + result);
+        }
+
+        static void Main(string[] args)
+        {
+           
+            Calculate(5, 3, Add);
+            Calculate(5, 3, Subtract);
+        }
+    }
+}
